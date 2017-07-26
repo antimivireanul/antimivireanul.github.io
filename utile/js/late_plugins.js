eb00d66af851a4f7fcc198d1cbd7d692a38305dd
eminote <emilnechifor@live.com>
2017-07-27 02:50:12 +0300

    adaugat unelte

---
+++ utile/js/late_plugins.js
@@ -0,0 +1,19 @@
+ <!-- PURE Menu -->
+<script>
+	(function (window, document) {
+	document.getElementById('toggle').addEventListener('click', function (e) {
+	  document.getElementById('tuckedMenu').classList.toggle('custom-menu-tucked');
+	  document.getElementById('toggle').classList.toggle('x');
+	});
+	})(this, this.document);
+</script>
+
+ <!-- Google Analytics -->
+<script>
+	(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+	function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+	e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+	e.src='//www.google-analytics.com/analytics.js';
+	r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+	ga('create','UA-103019958-1','auto');ga('send','pageview');
+</script>
