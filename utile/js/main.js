b1938c08109274ba090f71137377c34cef4d007d
antimivireanul <antimivireanul@mail.com>
2017-08-13 20:11:58 +0300

    corectari si adugat sageta in sus

--- utile/js/main.js
+++ utile/js/main.js
@@ -5,15 +5,20 @@ $(document).ready(function() {
       $body = $('body'),
       $window = $(window),
       navOffsetTop = $nav.offset().top
+	// browser window scroll (in pixels) after which the "back to top" link is shown
+	var offset = 300,
+		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+		offset_opacity = 1200,
+		//duration of the top scrolling animation (in ms)
+		scroll_top_duration = 700,
+		//grab the "back to top" link
+		$back_to_top = $('.cd-top');
 
   function init() {
     $window.on('scroll', onScroll)
     $window.on('resize', onScroll)
     
   }
-
-
-
 
   function resize() {
     $body.removeClass('has-docked-nav')
@@ -29,8 +34,20 @@ $(document).ready(function() {
     if(navOffsetTop > $window.scrollTop() && $body.hasClass('has-docked-nav')) {
       $body.removeClass('has-docked-nav')
     }
-	
+	( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+		if( $(this).scrollTop() > offset_opacity ) { 
+			$back_to_top.addClass('cd-fade-out');
+		}
   }
+
+  //smooth scroll to top
+	$back_to_top.on('click', function(event){
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+		 	}, scroll_top_duration
+		);
+	});
 
   init();
   
